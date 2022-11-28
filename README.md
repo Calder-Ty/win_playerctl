b6bee6e9c0169e5a2e2cd6d6faed87e7db76fb40
Tyler Calder <calder-ty@protonmail.com>
2022-11-28 15:03:32 -0700

    feat: add License and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+playerctl for windows!
+
+Ok, not quite. This is an overly simple tool that mimics playerctl's `metadata` command on windows.
+It allows me to maintain the same tmux.conf file that reports what I'm listening to in my statusline.
---
+++ src/main.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Tyler Calder, 2022
+
+// See https://aka.ms/new-console-template for more information
+using System;
+using Windows.Media.Control;
+
+class PlayerCTL
+{
+	static void Main(string[] args)
+	{
+		var sessionManager = GlobalSystemMediaTransportControlsSessionManager.RequestAsync().GetAwaiter().GetResult();
+		var currentSession = sessionManager.GetCurrentSession();
+		var mediaProperties = currentSession.TryGetMediaPropertiesAsync().GetAwaiter().GetResult();
+		if (args.Length != 2{
+			Console.WriteLine($"Playing {mediaProperties.Title} by {mediaProperties.Artist}") ;
+			return;
+		}
+		if ( args[1] == "title" ) {
+			Console.WriteLine($"{mediaProperties.Title}") ;
+			return;
+		} else if (args[1] == "artist") {
+			Console.WriteLine($"{mediaProperties.Artist}");
+			return;
+		}
+	}
+}
+
